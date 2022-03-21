90c56c6fe8db433951afcec28524d19e6aa234db
Konstantin <zkvprog@gmail.com>
2022-03-21 13:58:28 +0300

    Repeat String Command

--- app.php
+++ app.php
@@ -1,1 +1,9 @@
 <?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$app = new \Symfony\Component\Console\Application('demo application');
+
+$app->add(new \App\Console\RepeatString());
+
+$app->run();
